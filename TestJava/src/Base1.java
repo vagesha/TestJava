d377636d880946ea1dc50d885ea633aa7ad65d50
avenger <avenger@avenger>
2016-07-17 14:54:45 +0530

    test 1

--- TestJava/src/Base1.java
+++ TestJava/src/Base1.java
@@ -1,8 +1,21 @@
 
 public class Base1 {
 
+	public Base1 () {
+		System.out.println("Base1 construtor");
+	}
 	public void basePrint() {
 		System.out.println("Base void print");
+	}
+
+	public int fun(String string, int i) {
+		return i;
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public int fun(int i) {
+		return i+3;
 	}
 	
 }
--- TestJava/src/TestJava.java
+++ TestJava/src/TestJava.java
@@ -2,9 +2,19 @@
 public class TestJava {
 
 	public void testDerived() {
-		Base1 b = new Base1();
-		
-		b.basePrint();
+		//Base1 b = new Base1();
+		Base1 d = new Derived1();
+		//b.basePrint();
+		Derived1 d1 = new Derived1();
+		int i = d.fun("test", 1);
+		System.out.println(i);
+		i=d1.fun(1, "test");
+		System.out.println(i);
+		i=d.fun(1);
+		System.out.println(i);
+		i=d1.fun(1);
+		System.out.println(i);
+
 	}
 	
 	public static void main(String[] args) {
